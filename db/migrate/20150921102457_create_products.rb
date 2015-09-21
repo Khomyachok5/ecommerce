ffcd9beeb9aaac3f40bcbb15ce078827df2d5904
Vyacheslav Zenkevich <vyacheslav.zenkevich@teknavo.com>
2015-09-21 14:17:12 +0300

    Ensured the models work and made the necessary adjustments to migrations

--- db/migrate/20150921102457_create_products.rb
+++ db/migrate/20150921102457_create_products.rb
@@ -6,7 +6,7 @@ class CreateProducts < ActiveRecord::Migration
       t.text :description
       t.decimal :price
       t.integer :stock
-      t.boolean :deleted
+      t.boolean :deleted, default: false
 
       t.timestamps null: false
     end
