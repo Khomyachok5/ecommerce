3241eb3d4ffa2f19befa423796a6e7d6eb859b03
Vyacheslav Zenkevich <vyacheslav.zenkevich@teknavo.com>
2015-09-21 13:55:01 +0300

    Created the basis for product CRUD (products controller and resource, index view) as wel as Picture model

---
+++ db/migrate/20150921103209_create_pictures.rb
@@ -0,0 +1,10 @@
+class CreatePictures < ActiveRecord::Migration
+  def change
+    create_table :pictures do |t|
+      t.belongs_to :product, index: true
+      t.string :image
+
+      t.timestamps null: false
+    end
+  end
+end
