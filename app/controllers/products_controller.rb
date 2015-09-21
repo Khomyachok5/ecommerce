dd1714907e8c938d1d63e595df929c27f92fa832
Vyacheslav Zenkevich <vyacheslav.zenkevich@teknavo.com>
2015-09-21 16:55:51 +0300

    Added product creation action and modified the product partial

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,16 @@ class ProductsController < ApplicationController
     @product_listing = Product.order(created_at: :asc)
   end
 
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      flash.alert = @product.errors.full_messages.join('. ')
+      redirect_to new_product_path
+    end
+  end
+
   def show
   end
 
@@ -12,6 +22,10 @@ class ProductsController < ApplicationController
   end
 
   private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :price, :stock)
+  end
 
   def find_product
     @product = Product.friendly.find(params[:id])
