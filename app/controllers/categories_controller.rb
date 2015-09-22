05773e17f4ffdba3608913d727b53d54c6d4b2d2
Vyacheslav Zenkevich <vyacheslav.zenkevich@teknavo.com>
2015-09-22 23:12:13 +0300

    Added appropriate actions to create and update categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,14 +1,26 @@
 class CategoriesController < ApplicationController
   include Permissions
   before_action :check_privileges, only: [:create, :new, :update, :edit, :destroy]
+  before_action :find_category, only: [:edit, :update, :destroy]
   
   def new
   end
 
   def create
+    @category = Category.new(category_params)
+      if @category.save
+        flash.notice = "A new category has been successfully created"
+        redirect_to admin_path
+      else
+        flash.alert = @category.errors.full_messages.join('. ')
+        redirect_to admin_path
+      end
   end
 
   def update
+    @category.update_attributes(category_params)
+    flash.notice = "The category has been updated"
+    redirect_to admin_path  
   end
 
   def edit
@@ -17,5 +29,13 @@ class CategoriesController < ApplicationController
   def destroy
   end
 
+  private
 
+  def category_params
+    params.require(:category).permit(:title, :parent_category_id)
+  end
+
+  def find_category
+    @category = Category.find(params[:id])
+  end
 end
